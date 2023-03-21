12a953a9d3c603ea8de738672a01f497bd647f80
Kenny Leong <kennyleong2@gmail.com>
2023-03-21 19:37:39 -0400

    seed init file configured

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -2,6 +2,8 @@ from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .subreddits import seed_subreddits, undo_subreddits
 from .posts import seed_posts, undo_posts
+from .comments import seed_comments, undo_comments
+from .votes import seed_votes, undo_votes
 
 from app.models.db import db, environment, SCHEMA
 
@@ -18,18 +20,24 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_users()
-        undo_subreddits()
-        undo_posts()
+            undo_votes()
+            undo_comments()
+            undo_posts()
+            undo_subreddits()
+            undo_users()
     seed_users()
     seed_subreddits()
     seed_posts()
+    seed_comments()
+    seed_votes()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_votes()
+    undo_comments()
     undo_posts()
     undo_subreddits()
     undo_users()
