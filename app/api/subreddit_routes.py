9f8da4a3c00bff83396d6fa72e8bdc4aefcb78c9
Kenny Leong <kennyleong2@gmail.com>
2023-03-21 15:44:17 -0400

    imports added to subreddit route

---
+++ app/api/subreddit_routes.py
@@ -0,0 +1,48 @@
+from flask import Blueprint, jsonify, request, session
+from flask_login import login_required, current_user
+from app.models import Subreddit, Post, db
+
+subreddit_routes = Blueprint('subreddits', __name__)
+
+@subreddit_routes.route('')
+@login_required
+def subreddits():
+    """
+    Query for all subreddits and returns them in a list of subreddit dictionaries
+    """
+    subreddits = Subreddit.query.all()
+    return {'subreddits': [subreddit.to_dict() for subreddit in subreddits]}
+
+@subreddit_routes.route('/<int:id>')
+@login_required
+def subreddit(id):
+    """
+    Query for a subreddit by id and returns that subreddit in a dictionary
+    """
+    subreddit = Subreddit.query.get(id)
+    return subreddit.to_dict()
+
+@subreddit_routes.route('/<int:id>/posts')
+@login_required
+def subreddit_posts(id):
+    """
+    Query for all posts in a subreddit and returns them in a list of post dictionaries
+    """
+    posts = Post.query.filter_by(subreddit_id=id).all()
+    return {'posts': [post.to_dict() for post in posts]}
+
+
+@subreddit_routes.route('/<int:id>/create_post', methods=['POST'])
+@login_required
+def create_post_in_subreddit(id):
+    """
+    Create a new post in a subreddit with the current user as the author
+    """
+    data = request.get_json()
+    subreddit = Subreddit.query.get(id)
+    if not subreddit:
+        return jsonify(message='Invalid subreddit'), 400
+    post = Post(title=data['title'], content=data['content'], subreddit=subreddit, author=current_user)
+    db.session.add(post)
+    db.session.commit()
+    return post.to_dict()
