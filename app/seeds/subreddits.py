25de0db0dee24dcb356afedff6a3a5678b1f0695
Kenny Leong <kennyleong2@gmail.com>
2023-03-21 18:52:42 -0400

    subreddit seed data created

---
+++ app/seeds/subreddits.py
@@ -0,0 +1,36 @@
+from app.models import db, User, Subreddit, environment, SCHEMA
+from sqlalchemy.sql import text
+from datetime import datetime
+
+# Adds a demo subreddit, you can add other subreddits here if you want
+def seed_subreddits():
+    subreddits = [
+        Subreddit(name='AskReddit', description='Ask and answer questions'),
+        Subreddit(name='aww', description='Cute and cuddly animals'),
+        Subreddit(name='todayilearned', description='Learn something new every day'),
+        Subreddit(name='worldnews', description='News from around the world'),
+        Subreddit(name='funny', description='Funny memes and jokes')
+    ]
+
+    # Assign a creator to each subreddit
+    users = User.query.all()
+    for i in range(len(subreddits)):
+        subreddits[i].creator_id = users[i % len(users)].id
+        subreddits[i].created_at = datetime.utcnow()
+
+    db.session.add_all(subreddits)
+    db.session.commit()
+
+# Uses a raw SQL query to TRUNCATE or DELETE the subreddits table. SQLAlchemy doesn't
+# have a built in function to do this. With postgres in production TRUNCATE
+# removes all the data from the table, and RESET IDENTITY resets the auto
+# incrementing primary key, CASCADE deletes any dependent entities.  With
+# sqlite3 in development you need to instead use DELETE to remove all data and
+# it will reset the primary keys for you as well.
+def undo_subreddits():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.subreddits RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM subreddits"))
+
+    db.session.commit()
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,17 +4,18 @@ from sqlalchemy.sql import text
 
 # Adds a demo user, you can add other users here if you want
 def seed_users():
-    demo = User(
-        username='Demo', email='demo@aa.io', password='password')
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
-
-    db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
+    users = [
+        User(username='Demo', email='demo@aa.io', password='password'),
+        User(username='marnie', email='marnie@aa.io', password='password'),
+        User(username='bobbie', email='bobbie@aa.io', password='password'),
+        User(username='Kenny', email='kennyleong@gmail.com', password='password'),
+        User(username='JohnDoe', email='johndoe@example.com', password='password'),
+        User(username='JaneDoe', email='janedoe@example.com', password='password'),
+        User(username='MarkSmith', email='marksmith@example.com', password='password')
+    ]
+    db.session.add_all(users)
     db.session.commit()
+
 
 
 # Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
@@ -28,5 +29,5 @@ def undo_users():
         db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
     else:
         db.session.execute(text("DELETE FROM users"))
-        
+
     db.session.commit()
