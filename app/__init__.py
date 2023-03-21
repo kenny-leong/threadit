7b246bad832d74fd65e7ff18b73aba4f96ceae2d
Kenny Leong <kennyleong2@gmail.com>
2023-03-21 18:40:35 -0400

    routes all created successfully

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ from .api.auth_routes import auth_routes
 from .api.subreddit_routes import subreddit_routes
 from .api.post_routes import post_routes
 from .api.comment_routes import comment_routes
+from .api.vote_routes import vote_routes
 from .seeds import seed_commands
 from .config import Config
 
@@ -34,6 +35,7 @@ app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(subreddit_routes, url_prefix='/api/subreddits')
 app.register_blueprint(post_routes, url_prefix='/api/posts')
 app.register_blueprint(comment_routes, url_prefix='/api/comments')
+app.register_blueprint(vote_routes, url_prefix='/api/votes')
 
 db.init_app(app)
 Migrate(app, db)
