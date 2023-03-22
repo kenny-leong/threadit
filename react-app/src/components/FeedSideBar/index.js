1140e93266d6fdf30efdcb0fd2e02fc0643c6f5d
Kenny Leong <kennyleong2@gmail.com>
2023-03-22 15:57:07 -0400

    popular channel div added.

--- react-app/src/components/FeedSideBar/index.js
+++ react-app/src/components/FeedSideBar/index.js
@@ -10,7 +10,17 @@ function FeedSideBar() {
 
     return (
         <div className='feed-bar-div'>
-
+            <div className='feed-margin-div'>
+                <span className='feeds-text'>FEEDS</span>
+                <div className='home-channel-div'>
+                    <span><i class="fa-solid fa-house"></i></span>
+                    <span className='home-channel-text'>Home</span>
+                </div>
+                <div className='popular-channel-div'>
+                    <span><i class="fa-regular fa-fire"></i></span>
+                    <span className='popular-channel-text'>Popular</span>
+                </div>
+            </div>
         </div>
     )
 
