270abeb24c3442c9794d5739efff64419c010910
Kenny Leong <kennyleong2@gmail.com>
2023-03-22 13:03:11 -0400

    styling added to navbar

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -6,9 +6,21 @@ import './NavBar.css';
 
 function NavBar() {
 
+
+
 	return (
 		<div className='nav-bar-container'>
 			<img src={logo} alt='logo' />
+			<input
+				className="nav-search-bar"
+				type="text"
+				id="search-bar"
+				name="search-bar"
+				placeholder='Search Threadit'
+				required
+			/>
+			<button className='signup-btn'>Sign Up</button>
+			<button className='login-btn'>Log In</button>
 		</div>
 	);
 }
