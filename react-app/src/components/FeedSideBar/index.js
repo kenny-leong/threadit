2bfdd6ec2012cb95b168bc2b9a94a5e3bd197c93
Kenny Leong <kennyleong2@gmail.com>
2023-03-22 16:35:53 -0400

    sidebar 90% complete

--- react-app/src/components/FeedSideBar/index.js
+++ react-app/src/components/FeedSideBar/index.js
@@ -21,6 +21,42 @@ function FeedSideBar() {
                     <span className='popular-channel-text'>Popular</span>
                 </div>
             </div>
+            <div className='topic-margin-div'>
+                <span className='topics-text'>TOPICS</span>
+                <div className='gaming-channel-div'>
+                    <span><i class="fa-brands fa-twitch"></i></span>
+                    <span className='home-channel-text'>Gaming</span>
+                </div>
+                <div className='sports-channel-div'>
+                    <span><i class="fa-solid fa-basketball"></i></span>
+                    <span className='home-channel-text'>Sports</span>
+                </div>
+                <div className='business-channel-div'>
+                    <span><i class="fa-solid fa-coins"></i></span>
+                    <span className='home-channel-text'>Business, Economics, a..</span>
+                </div>
+                <div className='crypto-channel-div'>
+                    <span><i class="fa-brands fa-bitcoin"></i></span>
+                    <span className='home-channel-text'>Crypto</span>
+                </div>
+                <div className='tv-channel-div'>
+                    <span><i class="fa-solid fa-tv"></i></span>
+                    <span className='home-channel-text'>Television</span>
+                </div>
+                <div className='celeb-channel-div'>
+                    <span><i class="fa-regular fa-star"></i></span>
+                    <span className='home-channel-text'>Celebrity</span>
+                </div>
+                <div className='more-topics-div'>
+                    <span><i class="fa-solid fa-ellipsis"></i></span>
+                    <span className='home-channel-text'>More Topics</span>
+                </div>
+            </div>
+            <div className='join-threadit-div'>
+                <span className='join-text'>
+                Create an account to follow your favorite communities and start taking part in conversations.
+                </span>
+            </div>
         </div>
     )
 
