90413d6e0e70bac00afbd327157b6708042e61b3
Kenny Leong <kennyleong2@gmail.com>
2023-03-22 22:38:48 -0400

    adjustable join div for screen

--- react-app/src/components/FeedSideBar/index.js
+++ react-app/src/components/FeedSideBar/index.js
@@ -54,16 +54,18 @@ function FeedSideBar() {
                     <span className='home-channel-text'>More Topics</span>
                 </div>
             </div>
-            <div className='join-threadit-div'>
-                <span className='join-text'>
-                Create an account to follow your favorite communities and start taking part in conversations.
-                </span>
-            </div>
-            <div className='join-now-btn'>
-                <OpenModalButton
-                    buttonText={<span className='join-now-text'>Join Threadit</span>}
-                    modalComponent={<SignupFormPage />}
-                />
+            <div className='join-div'>
+                <div className='join-threadit-div'>
+                    <span className='join-text'>
+                    Create an account to follow your favorite communities and start taking part in conversations.
+                    </span>
+                </div>
+                <div className='join-now-btn'>
+                    <OpenModalButton
+                        buttonText={<span className='join-now-text'>Join Threadit</span>}
+                        modalComponent={<SignupFormPage />}
+                    />
+                </div>
             </div>
         </div>
     )
