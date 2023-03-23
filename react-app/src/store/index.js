930a44e0f3688385fa75a7138cc300e33c480bc9
Kenny Leong <kennyleong2@gmail.com>
2023-03-23 15:21:13 -0400

    subreddit reducer built

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import sessionReducer from './session'
+import subredditReducer from './subreddit';
 
 const rootReducer = combineReducers({
   session: sessionReducer,
+  subreddit: subredditReducer,
 });
 
 
--- react-app/src/store/subreddit.js
+++ react-app/src/store/subreddit.js
@@ -17,23 +17,32 @@ const loadSubredditPosts = (posts) => ({
     posts
 });
 
-const addPostToSubreddit = (post) => ({
-    type: 'ADD_POST_TO_SUBREDDIT',
-    post
-});
+// const addPostToSubreddit = (post) => ({
+//     type: 'ADD_POST_TO_SUBREDDIT',
+//     post
+// });
 
 const addSubreddit = (subreddit) => ({
     type: 'ADD_SUBREDDIT',
     subreddit
 });
 
+const removeSubreddit = (subredditId) => ({
+    type: 'REMOVE_SUBREDDIT',
+    subredditId,
+  });
+
+const updateSubreddit = (subreddit) => ({
+    type: 'UPDATE_SUBREDDIT',
+    subreddit
+});
 
 
 
 // -------------------------------------- thunk action creators -----------------------------------
 
 
-// LOADS ALL SUBREDDITS
+// GET ALL SUBREDDITS
 export const getAllSR = () => async (dispatch) => {
     const res = await fetch('/api/subreddits');
 
@@ -44,7 +53,7 @@ export const getAllSR = () => async (dispatch) => {
 };
 
 
-// LOADS ONE SUBREDDIT BY ID
+// GET SPECIFIC SUBREDDIT BY SUBREDDIT ID
 export const getSingleSR = (subredditId) => async (dispatch) => {
     const res = await fetch(`/api/subreddits/${subredditId}`);
 
@@ -54,7 +63,7 @@ export const getSingleSR = (subredditId) => async (dispatch) => {
     }
 };
 
-// LOADS ALL POSTS FOR A GIVEN SUBREDDIT ID
+// GETS ALL POSTS FOR A GIVEN SUBREDDIT ID
 export const getSubredditPosts = (subredditId) => async (dispatch) => {
     const res = await fetch(`/api/subreddits/${subredditId}/posts`);
 
@@ -65,23 +74,7 @@ export const getSubredditPosts = (subredditId) => async (dispatch) => {
 };
 
 
-// CREATE A POST WITHIN A SUBREDDIT
-export const createPostInSubreddit = (subredditId, title, content) => async (dispatch) => {
-    const res = await fetch(`/api/subreddits/${subredditId}/create_post`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ title, content }),
-    });
-
-    if (res.ok) {
-      const post = await res.json();
-      dispatch(addPostToSubreddit(post));
-    }
-};
-
-
+// CREATE A NEW SUBREDDIT
 export const createSubreddit = ({ name, description, profile_picture, banner_image }) => async dispatch => {
     const res = await fetch('/api/subreddits', {
         method: 'POST',
@@ -97,11 +90,77 @@ export const createSubreddit = ({ name, description, profile_picture, banner_ima
     });
 
     if (res.ok) {
-      const newSubreddit = await res.json();
-      dispatch(addSubreddit(newSubreddit));
-      return newSubreddit;
+        const newSubreddit = await res.json();
+        dispatch(addSubreddit(newSubreddit));
+        return newSubreddit;
+    } else if (res.status < 500) {
+        const data = await res.json();
+        if (data.errors) return data.errors
+    } else {
+        return ['An error occurred. Please try again.']
     }
 };
+
+
+// UPDATE AN EXISTING SUBREDDIT
+export const editSubreddit = ({ id, name, description, profile_picture, banner_image }) => async dispatch => {
+    const res = await fetch(`/api/subreddits/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name,
+            description,
+            profile_picture,
+            banner_image,
+        })
+    });
+
+    if (res.ok) {
+        const updatedSubreddit = await res.json();
+        dispatch(updateSubreddit(updatedSubreddit));
+        return updatedSubreddit;
+    } else if (res.status < 500) {
+        const data = await res.json();
+        if (data.errors) return data.errors
+    } else {
+        return ['An error occurred. Please try again.']
+    }
+};
+
+
+// DELETE A SUBREDDIT
+export const deleteSubreddit = (subredditId) => async (dispatch) => {
+    const res = await fetch(`/api/subreddits/${subredditId}`, {
+        method: 'DELETE',
+    });
+
+    if (res.ok) {
+      dispatch(removeSubreddit(subredditId));
+    }
+};
+
+
+
+
+
+
+// CREATE A POST WITHIN A SPECIFIC SUBREDDIT
+// export const createPostInSubreddit = (subredditId, title, content) => async (dispatch) => {
+//     const res = await fetch(`/api/subreddits/${subredditId}/create_post`, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({ title, content }),
+//     });
+
+//     if (res.ok) {
+//       const post = await res.json();
+//       dispatch(addPostToSubreddit(post));
+//     }
+// };
 
 
 
@@ -109,3 +168,47 @@ export const createSubreddit = ({ name, description, profile_picture, banner_ima
 
 
 const initialState = {}
+
+
+
+const subredditReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'LOAD_SUBREDDITS':
+            const allSubreddits = {};
+            const subredditArr = action.subreddits;
+            subredditArr.forEach(subreddit => {
+                allSubreddits[subreddit.id] = subreddit
+            });
+            return {
+                ...state,
+                allSubreddits: allSubreddits
+            }
+        case 'LOAD_SINGLE_SR':
+            const oneSubreddit = action.subreddit;
+            return {
+                ...state,
+                singleSubreddit: oneSubreddit
+            }
+        case 'ADD_SUBREDDIT':
+            return {
+                ...state,
+                singleSubreddit: {
+                    ...action.subreddit
+                }
+            }
+        case 'UPDATE_SUBREDDIT':
+            return {
+                ...state,
+                singleSubreddit: {
+                    ...action.subreddit
+                }
+            }
+        case 'REMOVE_SUBREDDIT':
+            const newState = {...state};
+            return newState;
+        default:
+            return state;
+    }
+}
+
+export default subredditReducer;
