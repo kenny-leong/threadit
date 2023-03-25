455fbe2ca3e7b5e2bdd15afe70317233c6b69419
Kenny Leong <kennyleong2@gmail.com>
2023-03-24 21:18:37 -0400

    create sr modal updated

--- react-app/src/components/CreateSubreddit/index.js
+++ react-app/src/components/CreateSubreddit/index.js
@@ -1,14 +1,51 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
+import './CreateSubreddit.css'
 
 
 
 function CreateSubreddit() {
 
+    const [name, setName] = useState("");
+    const [charactersLeft, setCharactersLeft] = useState(16);
 
+    useEffect(() => {
+        setCharactersLeft(16 - name.length);
+    }, [name]);
 
     return (
-        <></>
+        <div className='create-sr-div'>
+            <div className='width-adjustment-div'>
+                <div className='create-sr-heading-div'>
+                    <span className='create-sr-heading'>Create a Community</span>
+                </div>
+                <div className='create-sr-name-div'>
+                    <span className='create-sr-name'>Name</span>
+                    <span className='name-tos'>Community names including capitalization cannot be changed.</span>
+                </div>
+                <span className='placeholder-text'>r/</span>
+                <input
+                    type="text"
+                    value={name}
+                    placeholder='Name'
+                    className='sr-input-name'
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                />
+                <div className='char-left-div'>
+                    <span className='characters-left-text'>{`${charactersLeft} characters left`}</span>
+                </div>
+                <p className='create-sr-type'>Community Type</p>
+                <div className="radio-buttons-container">
+                    <input type="radio" id="text" name="channel-type" value="text" checked/>
+                    <div className="radio-button">
+                        <i class="fa-solid fa-user"></i>
+                        <span className="text-radio">Public</span>
+                        <span className="text-radio-desc">Anyone can view, post, and comment to this community</span>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 
 
