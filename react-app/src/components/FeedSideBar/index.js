3d6f295547b86071bc0e9d0be972e1908666985e
Kenny Leong <kennyleong2@gmail.com>
2023-03-27 01:06:52 -0400

    home functionality added.

--- react-app/src/components/FeedSideBar/index.js
+++ react-app/src/components/FeedSideBar/index.js
@@ -15,10 +15,12 @@ function FeedSideBar() {
         <div className='feed-bar-div'>
             <div className='feed-margin-div'>
                 <span className='feeds-text'>FEEDS</span>
-                <div className='home-channel-div'>
-                    <span><i class="fa-solid fa-house"></i></span>
-                    <span className='home-channel-text'>Home</span>
-                </div>
+                <Link to='/home'>
+                    <div className='home-channel-div'>
+                        <span><i class="fa-solid fa-house"></i></span>
+                        <span className='home-channel-text'>Home</span>
+                    </div>
+                </Link>
                 <div className='popular-channel-div'>
                     <span><i class="fa-solid fa-arrow-trend-up"></i></span>
                     <span className='popular-channel-text'>Popular</span>
