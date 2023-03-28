33834a111a486da73e60d60bc9e5fb0b17e4dc1b
Kenny Leong <kennyleong2@gmail.com>
2023-03-28 19:00:50 -0400

    added text content into viewing area for postfeed component

--- react-app/src/components/PostFeed/index.js
+++ react-app/src/components/PostFeed/index.js
@@ -112,6 +112,11 @@ function PostFeed() {
                             )}
                         </div>
                         <span className='feed-post-title'>{post.title}</span>
+                        {(post.content) && (
+                            <div className='feed-post-text-content'>
+                                <span className='post content'>{post.content}</span>
+                            </div>
+                        )}
                         {(post.image_url) && (
                             <div className='feed-post-div'>
                                 <img className='feed-post-img' alt='post-img' src={post.image_url} />
