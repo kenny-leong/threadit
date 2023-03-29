6b61d96ec2dd401665cbb8dbfc392e144741b8d8
Kenny Leong <kennyleong2@gmail.com>
2023-03-28 21:46:29 -0400

    community section z index adjusted

--- react-app/src/components/CreatePost/index.js
+++ react-app/src/components/CreatePost/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { useModal } from "../../context/Modal";
 import { createPost, getSubredditPosts } from '../../store/post';
