5ac6657abf3cfa4a3fff5f46f145ef7950811617
Kenny Leong <kennyleong2@gmail.com>
2023-03-30 19:38:47 -0400

    seed data more posts generated

--- app/seeds/comments.py
+++ app/seeds/comments.py
@@ -1,8 +1,14 @@
 from app.models import db, User, Post, Comment, environment, SCHEMA
 from sqlalchemy.sql import text
 from datetime import datetime
+from random import randint
+from faker import Faker
+
 
 def seed_comments():
+
+    fake = Faker()
+
     users = User.query.all()
     posts = Post.query.all()
     comments = [
@@ -13,6 +19,13 @@ def seed_comments():
         Comment(content='I found this article really interesting', author=users[1], post=posts[2], created_at=datetime.utcnow()),
         Comment(content='This is a great resource for beginners', author=users[2], post=posts[2], created_at=datetime.utcnow())
     ]
+
+    for post in posts:
+        num_comments = randint(15, 30)
+        for i in range(num_comments):
+            comment = Comment(content=fake.paragraph(), author=users[randint(0, len(users)-1)], post=post, created_at=fake.date_time_between(start_date='-1y', end_date='now'))
+            comments.append(comment)
+
     db.session.add_all(comments)
     db.session.commit()
 
